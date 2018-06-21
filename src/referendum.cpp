cb40e9f7ae5231bb0bc22b34470476435de27412
Michael <Fletch153@hotmail.com>
2018-06-21 14:20:40 +0100

    Altered structure. Votes now counted at EOD

--- src/referendum.cpp
+++ src/referendum.cpp
@@ -9,92 +9,62 @@
 
 #include "referendum.hpp"
 
-/*TODO User can vote, unstake, move tokens and revote. We need to understand how this is dealt with when voting for BP's and how we can resolve */
-
 namespace referendum {
-
-void referendum::init(account_name self){
-
-  require_auth(self);
-
-  /* initalise tables */
-  results.emplace(self, [&](auto &result_rec){
-    result_rec.id = 0; //no vote
-  });
-
-  results.emplace(self, [&](auto &result_rec){
-    result_rec.id = 1; //yes vote
-  });
-  
-}
 
 void referendum::vote(account_name voter_name, uint8_t vote_side){
   require_auth(voter_name);
 
-  auto voter = voters.find(voter_name);
+  /* TODO Check if vote is active */
 
   /* if they've staked, their will be a voter entry */
-  eosio_assert(voter != voters.end(), "user must stake before they can vote.");
+  auto voter = voter_info.find(voter_name);
+  eosio_assert(voter == voter_info.end(), "user must stake before they can vote.");
 
   /* vote side yes = 1 or no = 0 */
   eosio_assert(validate_side(vote_side), "vote side is invalid");
 
-   /* if a vote has been cast previously, check if the new vote is voting for the same side. If so - update the quantity */   
-  if(voter->existing_vote_side != NULL_VOTE){
-    eosio_assert(voter->existing_vote_side == vote_side, "cannot vote both yes and no at the same time.");
-  }
+  /* have they already voted */
+  auto registered_voter = registered_voters.find(voter_name);
+  eosio_assert(registered_voter == registered_voters.end(), "user has already voted"); 
 
-   /* update the quantity if they've staked more, or if this is the users first time staking */
-  double update_delta_quantity = voter->staked - voter->existing_vote_quantity;
-
-  eosio_assert(update_delta_quantity == 0, "user has already voted their staked tokens");
- 
-   /* apply delta qty */   
-  voters.modify(voter, _self, [&](auto &vote_rec){
-      vote_rec.existing_vote_quantity = update_delta_quantity;
+  /* register vote */
+  registered_voters.emplace(_self, [&](auto &voter_rec){
+    voter_rec.name = voter_name;
   });
-
-
-  /* first time voting */
-  if(voter->existing_vote_side != NULL_VOTE){
-    voters.modify(voter, _self, [&](auto &vote_rec){
-        vote_rec.existing_vote_side = vote_side;
-      });
-  }
-
-  /* get the vote side we're looking for */
-  auto result = results.find(vote_side);
-  eosio_assert(result != results.end(), "contract has not been initialised");
-
-  /* apply delta quantity */
-  results.modify(result, _self, [&](auto &result_rec){
-        result_rec.total_votes += update_delta_quantity;
-      });
+   
 }
-
 
 void referendum::unvote(account_name voter_name){
   require_auth(voter_name);
 
-  /* find the voter and check they've voted */
-  auto voter = voters.find(voter_name);
-  eosio_assert(voter->existing_vote_quantity != 0, "user must vote before they can unvote");
+  /*TODO Check if vote is active */
 
-  /* find the side they voted for */
-  auto result = results.find(voter->existing_vote_side);
-  eosio_assert(result != results.end(), "contract has not been initialised");
+   /* have they voted */
+  auto registered_voter = registered_voters.find(voter_name);
+  eosio_assert(registered_voter != registered_voters.end(), "user has not voted");
 
-  /* reduce the votes */
-  results.modify(result, _self, [&](auto &result_rec){
-        result_rec.total_votes -= voter->existing_vote_quantity;
-      });
+  /* remove the user */
+  registered_voters.erase(registered_voter);
+}
 
-  /* null the voter */
-  voters.modify(voter, _self, [&](auto &vote_rec){
-      vote_rec.existing_vote_quantity = 0;
-      vote_rec.existing_vote_side = NULL_VOTE;
-  });
+void referendum::countvotes(account_name publisher){
+  require_auth(publisher);
 
+  /* check the vote is active */
+
+  
+  /* count the votes */
+  uint64_t total_votes = 0;
+
+  /* if vote_count / total_eos * 100 > MINIMUM_VOTE_PARTICIPATION_PERCENTAGE, this periods vote is succesful */
+
+  /* if not, check if there's enough time left in this period to finish the vote */
+
+  /* if true, start again at 0 */
+
+  /* if false, finish the vote */
+ 
+  /* increment total days passed */ 
 }
 
 bool referendum::validate_side(uint8_t vote_side){
--- src/referendum.hpp
+++ src/referendum.hpp
@@ -8,57 +8,53 @@
  */
 
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/singleton.hpp>
 #include <eosio.system/eosio.system.hpp>
 #include "referendum_declarations.hpp"
 
 namespace referendum {
 
 
-struct extended_voter_info : public eosiosystem::voter_info {
+/*TODO Change scope to point to system voters table? */
+typedef eosio::multi_index<N(voters), eosiosystem::voter_info> voter_info_table;
 
-  double existing_vote_quantity = 0;
-  uint8_t existing_vote_side = NULL_VOTE; 
 
-  EOSLIB_SERIALIZE_DERIVED(extended_voter_info, eosiosystem::voter_info, (existing_vote_quantity)(existing_vote_side))
+struct referendum_info{
+   uint64_t total_days = 0; // total days passed
+   uint64_t total_c_days = 0; // total consecutive days vote has passed
+   bool	    vote_active = true; // false when the vote has finished
+
+   EOSLIB_SERIALIZE(referendum_info, (total_days)(total_c_days))
 };
+typedef eosio::singleton<N(referendum), referendum_info> referendum_results_table;
 
-typedef eosio::multi_index<N(voters), extended_voter_info> extended_voter_table;
+ 
+struct registered_voters {
 
-
-struct result {
-
-    int64_t id;
-    double  total_votes = 0;
-
+    account_name name;
+    
     uint64_t primary_key() const {
-        return id;
+        return name;
     }
 
-    double by_votes() const {
-        return total_votes;
-    }
-
-    EOSLIB_SERIALIZE(result, (id)(total_votes));
+    EOSLIB_SERIALIZE(registered_voters, (name));
 };
-
-typedef eosio::multi_index< N(results), result,
-        eosio::indexed_by<N(totalvotes), eosio::const_mem_fun<result, double, &result::by_votes>  >
-        >  results_table;
+typedef eosio::multi_index<N(refvoters), registered_voters>  registered_voters_table;
 
 
 class referendum : public eosio::contract {
-
 public:
-    referendum(account_name self):contract(self), voters(self, self), results(self, self) {}
+    referendum(account_name self):contract(self), registered_voters(self, self), voter_info(self, self), referendum_results(self, self) {}
   
-    void init(account_name self);
     void vote(account_name voter, uint8_t vote_side);
     void unvote(account_name voter);
+    void countvotes(account_name publisher);
 
 private:
-    extended_voter_table  voters;
-    results_table	   results;
-
+    registered_voters_table    registered_voters;
+    voter_info_table	       voter_info;
+    referendum_results_table   referendum_results;
+ 
     bool validate_side(uint8_t vote_side);
 };
 
